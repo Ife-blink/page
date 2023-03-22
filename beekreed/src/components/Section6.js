7ad5d3f9cab6eac31b613c2bcb46fbdb7ecdbcee
Ife Asiadiachi <iasiadiachi@gmail.com>
2023-03-22 00:58:46 +0000

    section animation

--- beekreed/src/components/Section6.js
+++ beekreed/src/components/Section6.js
@@ -5,59 +5,65 @@ import Image from 'next/image'
 function Section6() {
 
     const picvariant = {
-        // hidden: { opacity: 1, y: "2rem"},
-        // visible: {y: 0, opacity: 1,
-        //   transition:{ duration: 3, repeat: Infinity }
-        // },
-        // hiddenTwo: { opacity: 0.3, y:0, x: 0},
-        // visibleTwo: {x: "15rem", opacity: 1,
-        //   transition:{ duration: 3, repeat: Infinity }
-        // },
-        // visibleThree: {x: "-15rem", opacity: 1,
-        //   transition:{ duration: 3, repeat: Infinity }
-        // }
+        hidden: { opacity: 1, y: "3rem"},
+        visible: {y: 0, opacity: 1,
+          transition:{ duration: 2, repeat: Infinity }
+        },
+        hiddenTwo: { opacity: 1, x: 0},
+        visibleTwo: {x: "15rem", opacity: 1,
+          transition:{ duration: 2, repeat: Infinity }
+        },
+        visibleThree: {x: "-15rem", opacity: 1,
+          transition:{ duration: 2, repeat: Infinity }
+        },
+        visibleFour: {x: "-30rem", opacity: 1,
+        transition:{ duration: 2, repeat: Infinity }
+        },
+        visibleFive: {x: "30rem", opacity: 1,
+        transition:{ duration: 2, repeat: Infinity }
+        },
       }
 
   return (
     <section className='h-[100vh] w-[100%] bg-black text-white'>
 
       
-      <div className="w-[100%]  h-[80%] py-[5%] overflow-hidden relative bg-green-400 flex justify-center items-center">
+      <div className="w-[100%] h-[80%] py-[5%] overflow-hidden relative bg-purple-600 flex justify-center items-center">
       <motion.div 
         initial="hidden"
-        animate="visibleThree"
+        animate="visibleFour"
         variants={picvariant}
-        className=" h-[20rem] w-[20rem] flex justify-center items-center z-0 bg-blue-600">
-       <Image src="AnimationBlocks/pic3.svg" width={200} height={200} />
+        className=" absolute h-[20rem] w-[20rem] flex justify-center items-center z-3 ">1
+       <Image src="AnimationBlocks/pic1.svg" width={200} height={200} />
          </motion.div>
          <motion.div 
         initial="hidden"
         animate="visibleThree"
         variants={picvariant}
-        className=" h-[20rem] w-[20rem] flex justify-center items-center z-0 bg-blue-600">
-       <Image src="AnimationBlocks/pic5.svg" width={200} height={200} />
+        className=" absolute h-[20rem] w-[20rem] flex justify-center items-center z-1 ">2
+        <Image src="AnimationBlocks/pic4.svg" width={200} height={200} />
          </motion.div>
         
         <motion.div
           initial="hidden"
           animate="visible"
          variants={picvariant}
-        className=" h-[20rem] w-[20rem] flex justify-center items-center absolute z-2  bg-purple-600">
-        <Image src="AnimationBlocks/pic1.svg" width={200} height={200} />
+        className=" absolute h-[20rem] w-[20rem] flex justify-center items-center z-2 ">3
+        <Image src="AnimationBlocks/pic3.svg" width={200} height={200} />
           </motion.div>
           <motion.div
           initial="hidden"
-          animate="visible"
+          animate="visibleFive"
          variants={picvariant}
-        className=" h-[20rem] w-[20rem] flex justify-center items-center absolute z-2  bg-purple-600">
-        <Image src="AnimationBlocks/pic4.svg" width={200} height={200} />
+        className=" h-[20rem] w-[20rem] flex justify-center items-center absolute z-4 ">4
+        <Image src="AnimationBlocks/pic5.svg" width={200} height={200} />
           </motion.div>
 
         <motion.div 
-        initial="hiddenTwo"
+        initial="hidden"
         animate="visibleTwo"
         variants={picvariant}
-        className=" h-[20rem] w-[20rem] flex justify-center items-center absolute z-0 bg-blue-600">
+        className=" h-[20rem] w-[20rem] flex justify-center items-center absolute z-0 ">5
        <Image src="AnimationBlocks/pic2.svg" width={200} height={200} />
          </motion.div>
        

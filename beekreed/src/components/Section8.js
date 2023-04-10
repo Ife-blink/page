46fc2058575a15d249ece4cb28e20c73d70c6189
A9d <108765381+Ife2002@users.noreply.github.com>
2023-04-10 05:40:14 +0000

    Evian dey

--- beekreed/src/components/Section8.js
+++ beekreed/src/components/Section8.js
@@ -5,10 +5,20 @@ function Section8() {
   return (
     <div className='text-black h-[100vh] w-[100%]'>
         <h1 className='font-semibold font-sans'>We help kini pe</h1>
-        <div>
-            <div className='h-[80%] w-[20%]'>
-            <Image fill src={`http://source.unsplash.com/IeWqFDcOz5E`} />
+        <div className='flex flex-row justify-between px-[5%]'>
+            <div className='h-[80%] w-[20%] bg-black rounded-[20px] overflow-hidden'>
+            <Image layout='responsive' height={600} width={300} src={`http://source.unsplash.com/IeWqFDcOz5E`} />
             </div>
+            <div className='h-[100%] w-[20%] bg-black rounded-[20px] relative overflow-hidden'>
+            <Image layout='responsive' fill src={`http://source.unsplash.com/qj15uNotnH0`} />
+            </div>
+            <div className='h-[80%] w-[20%] bg-black rounded-[20px] overflow-hidden'>
+            <Image layout='responsive' height={600} width={300} src={`http://source.unsplash.com/4YMrFvN45L4`} />
+            </div>
+            <div className='h-[80%] w-[20%] bg-black rounded-[20px] overflow-hidden'>
+            <Image layout='responsive' height={600} width={300} src={`http://source.unsplash.com/csJt89dL9pE`} />
+            </div>
+            
         </div>
     </div>
   )
